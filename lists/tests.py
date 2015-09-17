1678f6bb4d0b8cc21f15c02069deba94654b1d98
Rian Fitriansyah <rian.fitriansyah@ui.ac.id>
2015-09-17 04:02:17 +0000

    editing functional test

--- lists/tests.py
+++ lists/tests.py
@@ -34,7 +34,7 @@ class HomePageTest(TestCase):
    def test_home_page_returns_correct_html(self):
        request = HttpRequest() #
        response = home_page(request) #
-       expected_html = render_to_string('home.html')
+       expected_html = render_to_string('home.html', {'comment':'yey, waktunya berlibur'})
        self.assertEqual(response.content.decode(), expected_html)
        # self.assertTrue(response.content.startswith(b'<html>')) #
        # self.assertIn(b'<title>To-Do lists</title>', response.content) #
@@ -75,3 +75,33 @@ class HomePageTest(TestCase):
 
        self.assertIn('itemey 1', response.content.decode())
        self.assertIn('itemey 2', response.content.decode())
+
+   def test_item_kosong(self):
+       request = HttpRequest()
+       response = home_page(request)
+
+       self.assertEqual(Item.objects.count(), 0)
+       self.assertIn('yey, waktunya berlibur', response.content.decode())
+       
+   def test_item_kurang_dari_lima(self):
+       Item.objects.create(text='itewey 1')
+
+       request = HttpRequest()
+       response = home_page(request)
+
+       self.assertGreater(Item.objects.count(), 0)
+       self.assertLess(Item.objects.count(), 5)
+       self.assertIn('sibuk tapi santai', response.content.decode())
+
+   def test_item_greater_than_five(self):
+       Item.objects.create(text='itewey 1')
+       Item.objects.create(text='itewey 2')
+       Item.objects.create(text='itewey 3')
+       Item.objects.create(text='itewey 4')
+       Item.objects.create(text='itewey 5')
+
+       request = HttpRequest()
+       response = home_page(request)
+
+       self.assertGreaterEqual(Item.objects.count(), 5)
+       self.assertIn('oh tidak', response.content.decode())
--- lists/views.py
+++ lists/views.py
@@ -8,4 +8,14 @@ def home_page(request):
        return redirect('/')
 
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    list_item = Item.objects.count()
+    comment = ''
+
+    if list_item == 0: 
+       comment = 'yey, waktunya berlibur'
+    elif list_item < 5: 
+       comment = 'sibuk tapi santai'
+    else: 
+       comment='oh tidak'
+
+    return render(request, 'home.html', {'comment':comment, 'items':items})
